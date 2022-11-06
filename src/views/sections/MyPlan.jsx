432baf002bd3bd74b57e18e7deb443a351c3e7c3
Woody <gintooooonic@gmail.com>
2022-11-07 05:16:28 +0900

    feat: MyPlan 섹션 추가

--- src/views/sections/MyPlan.jsx
+++ src/views/sections/MyPlan.jsx
@@ -1,5 +1,23 @@
+import { Section, SectionTitle, SectionDesc } from '../components/Section';
+
+import screenshot from '../../assets/images/screenshot-myplan.png';
+
 function MyPlan() {
-  return <section className="MyPlan"></section>;
+  return (
+    <Section className="MyPlan">
+      <div>
+        <SectionTitle>마이 플랜</SectionTitle>
+        <SectionDesc>
+          여행지 추천을 받았다면, 이제 일정을 짤 차례
+          <br />
+          여행 일정을 편리하고, 똑똑하게 관리해보세요!
+        </SectionDesc>
+      </div>
+      <div>
+        <img src={screenshot} alt="screenshot" />
+      </div>
+    </Section>
+  );
 }
 
 export default MyPlan;
